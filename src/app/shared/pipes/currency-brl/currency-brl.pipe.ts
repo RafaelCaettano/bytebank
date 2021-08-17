35c2823093bd6a7e21fec860212e91d66a9cc4bc
rafaelcaettano <rafac.docarmo@hotmail.com>
2021-08-17 17:00:57 -0300

    feat: criando pipe currencyBrl

---
+++ src/app/shared/pipes/currency-brl/currency-brl.pipe.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'currencyBrl'
+})
+export class CurrencyBrlPipe implements PipeTransform {
+
+  transform(value: number): string {
+    return value.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+  }
+
+}
