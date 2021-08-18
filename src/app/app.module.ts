ea79d2cb0e45b18218ccacf591f94f6178a9c4e0
rafaelcaettano <rafac.docarmo@hotmail.com>
2021-08-18 17:00:01 -0300

    fix: adicionando o modulo do store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './shared/state/state.index';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HeaderModule,
     HttpClientModule,
+    StoreModule.forRoot(reducers)
   ],
   providers: [],
   bootstrap: [AppComponent]
